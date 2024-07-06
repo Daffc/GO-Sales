3960b0e74c0f03ba914a05fb2973d115b3c8da02
Douglas A.C <douglas.affc@gmail.com>
2024-07-05 23:13:36 -0300

    Recovering environment configuration variables.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/Daffc/GO-Sales/internal/users"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	godotenv.Load()
 	http.HandleFunc("/users/{userId}", users.GetUserById)
 	http.HandleFunc("POST /users", users.PostUser)
-	fmt.Println("Linsten....")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Linstening on %s ...\n", os.Getenv("APP_PORT"))
+	http.ListenAndServe(":"+os.Getenv("APP_PORT"), nil)
 }
